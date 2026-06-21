2d5ba4bf7f4933207673c97b5d826b5bff58e3a8
poornesh <poorneshraja1@gmail.com>
2026-06-22 04:37:51 +0530

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
-import Navbar from '@/components/Navbar';
+import Navbar from '../components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
